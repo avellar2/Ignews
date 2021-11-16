92c2c200a62a60f89c388e3591bcf41e36b3fd10
Vanderson Avellar <vandimavellar1997@gmail.com>
2021-11-16 12:26:40 -0300

    first commit

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,23 @@
+import { FaGithub } from "react-icons/fa";
+import {FiX} from 'react-icons/fi'
+import styles from "./styles.module.scss";
+import {signIn, signOut, useSession} from 'next-auth/client'
+
+export function SignInButton() {
+    const [session] = useSession()
+
+    
+
+    return session ? (
+        <button type="button" className={styles.signInButton} onClick={()=> signOut()}>
+            <FaGithub color="#04d361"></FaGithub>
+                {session.user.name}
+            <FiX color='#737380' className={styles.closeIcon}></FiX>
+        </button>
+    ) : (
+        <button type="button" className={styles.signInButton} onClick={()=> signIn('github')}>
+            <FaGithub color="#eba417"></FaGithub>
+            Sign In with GitHub
+        </button>
+    );
+}
