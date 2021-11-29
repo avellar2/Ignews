b6ecfeb2871835343fe2c144d17de54c9480dd9e
Vanderson Avellar <vandimavellar1997@gmail.com>
2021-11-29 12:17:43 -0300

    6°commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,27 @@
-import { SignInButton } from '../SignInButton'
-import styles from './styles.module.scss'
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
+import { ActiveLink } from "../ActiveLink";
 
 export const Header = () => {
+
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="Logo" />
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <ActiveLink activeClassName={styles.active} href="/">
+                        <a>Home</a>
+                    </ActiveLink>
+
+                    <ActiveLink activeClassName={styles.active} href='/posts' prefetch>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
                 <SignInButton></SignInButton>
             </div>
         </header>
-    )
-} 
+    );
+};
